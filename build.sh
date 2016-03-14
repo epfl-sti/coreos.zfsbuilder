f820f5620c4c24cbd6b6f8d271ea41f9a9ebf68c
CoreOS Admin <core@c04.ne.cloud.epfl.ch>
2016-03-14 19:02:56 +0000

    Download the right tool chain
    "download" command works, but "build" is bogus. Instead, use https://gist.github.com/marineam/9914debc25c8d7dc458f

--- build.sh
+++ build.sh
@@ -2,33 +2,48 @@
 
 set -e -x
 
-DOCKERNAME=coreos.zfs-builder
+. /usr/share/coreos/release
+BUILD_TAG=coreos.zfs-builder.${COREOS_RELEASE_BOARD}-${COREOS_RELEASE_VERSION}
+cd "$(dirname "$0")"
+build_dir=build-${BUILD_TAG}
+
+
+mkdir_build() {
+  mkdir "$build_dir" ||true
+}
+
+download() {
+    mkdir_build
+    if [ -f /etc/coreos/update.conf ]; then
+        . /etc/coreos/update.conf
+        : ${COREOS_RELEASE_CHANNEL:=${GROUP}}
+    else
+        : ${COREOS_RELEASE_CHANNEL:=stable}
+    fi
+    if [ ! -f "$build_dir/coreos_developer_container.bin.bz2" ]; then
+        curl -o "$build_dir/coreos_developer_container.bin.bz2" http://${COREOS_RELEASE_CHANNEL}.release.core-os.net/${COREOS_RELEASE_BOARD}/${COREOS_RELEASE_VERSION}/coreos_developer_container.bin.bz2
+    fi
+}
 
 build() {
-  docker build -t epflsti/${DOCKERNAME} .
-}
+  download
+  cat > "$build_dir"/Dockerfile <<EOF
+FROM busybox
+MAINTAINER STI-IT Dev <stiitdev@groupes.epfl.ch>
 
-autorun() {
-  docker rm -f ${DOCKERNAME} 2>/dev/null || true
-  docker run \
-         --name ${DOCKERNAME} \
-         -v /usr/share/coreos:/host/usr/share/coreos \
-         epflsti/${DOCKERNAME} /build-zfs.sh
-}
+ADD coreos_developer_container.bin.bz2 /
+# RUN bzcat /coreos_developer_container.bin.bz2 | tar -xf - -C /
+EOF
 
-exec() {
-  docker exec -it \
-         --name ${DOCKERNAME} \
-         -v /usr/share/coreos:/host/usr/share/coreos \
-         epflsti/${DOCKERNAME} /bin/sh
+  docker build -t epflsti/${BUILD_TAG} "$build_dir"
 }
 
 interactive() {
-  docker rm -f ${DOCKERNAME} 2>/dev/null || true
+  docker rm -f ${BUILD_TAG} 2>/dev/null || true
   docker run -it \
-         --name ${DOCKERNAME} \
+         --name ${BUILD_TAG} \
          -v /usr/share/coreos:/host/usr/share/coreos \
-         epflsti/${DOCKERNAME} /bin/sh
+         epflsti/${BUILD_TAG} /bin/sh
 }
 
 eval "$@"
