a0a9614b86b76469a3b0c0a94c974d78b85c838f
CoreOS Admin <core@c69.ne.cloud.epfl.ch>
2016-05-22 10:02:53 +0000

    Improve to finish the downloading of the sources
    Tested to work with current Dockerfile

--- build-zfs.sh
+++ build-zfs.sh
@@ -3,12 +3,17 @@
 # https://gitlab.epfl.ch/sti-it/ops.nemesis/issues/1
 set -e -x
 
+setup_environment() {
+    KERNEL_RELEASE_MINOR=$(uname -r | cut -d. -f1-2)
+    # Obtain $DISTRIB_RELEASE:
+    . /host/usr/share/coreos/lsb-release
+    DISTRIB_RELEASE_MAJOR=$(echo "${DISTRIB_RELEASE}" |cut -d. -f1)
+}
 
 download_kernel_sources() {
-    . /host/usr/share/coreos/lsb-release
-    # From https://coreos.com/os/docs/latest/sdk-modifying-coreos.html:
+    # See https://coreos.com/os/docs/latest/sdk-modifying-coreos.html
+
     mkdir /coreos
-    
     # "| cat" prevents color autodetect monkey business
     (cd /coreos; repo init -u https://github.com/coreos/manifest.git -b refs/tags/v${DISTRIB_RELEASE}) |cat
     (cd /coreos; repo sync coreos/coreos-overlay)
@@ -17,9 +22,6 @@ download_kernel_sources() {
     . /coreos/.repo/manifests/version.txt
     OVERLAY_GIT_VERSION=$(perl -ne 'm|name="coreos/coreos-overlay".*revision="(.*?)"| && print $1' /coreos/.repo/manifests/build-${COREOS_BUILD}.xml)
     (cd /coreos/src/third_party/coreos-overlay; git checkout ${OVERLAY_GIT_VERSION})
-    return  # XXX
-
-    KERNEL_RELEASE_MINOR=$(uname -r | cut -d. -f1-2)
 
     git clone -b build-${DISTRIB_RELEASE_MAJOR} https://github.com/coreos/coreos-overlay.git
 }
@@ -36,4 +38,5 @@ build_zfs_sources() {
     :
 }
 
+setup_environment
 download_kernel_sources
