6e94c2281c1d23518397bfab87394f3d3cdba633
CoreOS Admin <core@c69.ne.cloud.epfl.ch>
2016-08-23 19:37:27 +0000

    Merge branch 'master' of github.com:epfl-sti/coreos.zfsbuilder

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -e -x
 
 DEPOT=registry.service.consul:5000
 DOCKERNAME=cluster.coreos.zfs
+. /usr/share/coreos/lsb-release
+DOCKER_IMAGE_THISVERSION=${DEPOT}/${DOCKERNAME}:${DISTRIB_RELEASE}
 
 build_base() {
   DOCKER_IMAGE_BASE=${DEPOT}/${DOCKERNAME}.base
@@ -14,17 +16,26 @@ build_base() {
 
 build_this_version() {
   cp /usr/share/coreos/lsb-release zfs/
-  . zfs/lsb-release
-  DOCKER_IMAGE_THISVERSION=${DEPOT}/${DOCKERNAME}:${DISTRIB_RELEASE}
   docker pull ${DOCKER_IMAGE_THISVERSION} || true
   docker build -t ${DOCKER_IMAGE_THISVERSION} zfs/
   docker push ${DOCKER_IMAGE_THISVERSION} || true
 }
 
+export_payload() {
+    docker run --rm -v $PWD:/export ${DOCKER_IMAGE_THISVERSION} \
+          tar zcf /export/linux-zfs-${DISTRIB_RELEASE}.tgz \
+          /usr/portage/packages/sys-fs/ /lib/modules
+}
+
+explore() {
+    docker run --rm -v $PWD:/export -it ${DOCKER_IMAGE_THISVERSION} /bin/bash
+}
+           
 if [ -n "$1" ]; then
     eval "$@"
 else
     build_base
     build_this_version
+    export_payload
 fi
 
